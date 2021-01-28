ffdcaac8d91731bc1a9fc0674b283017baa9c510
昕0127 <815396504@qq.com>
2021-01-28 22:27:11 +0800

    提交代码

---
+++ Test1-28/src/test.java
@@ -0,0 +1,15 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("ccc");
+        System.out.println("bbb");
+        int num = 10;
+        if (num > 0) {
+        System.out.println("num 是正数");
+        } else if (num < 0) {
+        System.out.println("num 是负数");
+        } else {
+        System.out.println("num 是 0");
+        }
+    }
+}
