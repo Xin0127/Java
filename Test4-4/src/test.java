851867dfb69ca86c0ef5b963cec4e32329fb6147
昕0127 <815396504@qq.com>
2021-04-04 16:16:53 +0800

    提交代码

---
+++ Test4-4/src/test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String str=scanner.nextLine();
+
+    }
+//    public static void main(String[] args) {
+//        Scanner scanner=new Scanner(System.in);
+//        int N=scanner.nextInt();
+//        int n1=0,n2=1;
+//        int n3;
+//        while(n2<N){
+//            n3=n1+n2;
+//            n1=n2;
+//            n2=n3;
+//        }
+//        System.out.println(Math.min((n2 - N), (N - n1)));
+//    }
+
+}
