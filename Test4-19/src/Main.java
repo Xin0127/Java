e455b41632ffffd722b9848aaec1c1881164ff1a
昕0127 <815396504@qq.com>
2021-04-20 17:47:17 +0800

    java

---
+++ Test4-19/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.round(11.5));
+
+//        最小公倍数
+//        Scanner scanner=new Scanner(System.in);
+//        int i=scanner.nextInt();
+//        int j=scanner.nextInt();
+//        int max = Integer.max(i, j);
+//        for (int n = max; n <= i*j; n++) {
+//            if (n % i == 0 && n % j == 0) {
+//                System.out.println(n);
+//                break;
+//            }
+//        }
+
+
+//        int count=0;
+//        System.out.println("请输入网络格子的长和宽（W>=1,H<=1000)");
+//        Scanner scanner=new Scanner(System.in);
+//        int H=scanner.nextInt();
+//        int W=scanner.nextInt();
+//        for(int x1=0;x1<=H-1;x1++){
+//            for(int y1=0;y1<=W-1;y1++){
+//                for(int x2=x1;x2<=H-1;x2++){
+//                    for(int y2=y1;y2<=W-1;y2++){
+//                        if((((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))!=4)&&(x1!=x2)&&(y1!=y2)){
+//                            count+=2;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println(count);
+    }
+}
