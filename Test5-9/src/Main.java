63ef9d6efadbc6deb50ef69126a0845f9def1320
昕0127 <815396504@qq.com>
2021-05-09 14:31:32 +0800

    java

---
+++ Test5-9/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("输入月份：");
+        Scanner scanner=new Scanner(System.in);
+        int n= scanner.nextInt();
+        System.out.println(getTotalCount(n));
+    }
+
+    private static int getTotalCount(int monthCount) {
+        int sum=1;
+        //1-1 2-1 3-2 4-3
+        for(int i=1;i<=monthCount;i++){
+            sum+=i;
+        }
+        return sum;
+
+    }
+}
